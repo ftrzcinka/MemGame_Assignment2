f391d2d5f76446b5fa3373dba25812158dd274ec
ftrzcinka <80350380+ftrzcinka@users.noreply.github.com>
2023-03-22 20:26:37 -0400

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import { useState, useRef, useEffect } from 'react'
+import MemCards from './Components/MemCards'
+
+function App() {
+
+    return (
+        <div className="App">
+            <h1>Elden Ring Memory Game</h1>
+            <MemCards />
+        </div>
+    );
+}
+
+
+export default App;
