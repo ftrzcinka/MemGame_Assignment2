4388d7183f8afde61573d1e495fa3d7a9f407034
ftrzcinka <80350380+ftrzcinka@users.noreply.github.com>
2023-03-25 15:35:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import MemCards from './Components/MemCards'
 
 function App() {
 
+    const [moveCounter, setMoveCounter] = useState(0)
+    const [completion, setCompletion] = useState(0)
+
     function newGame() {
         window.location.reload(false)
+    }
+
+    function gameStatus(completion) {
+        if (completion == 6) {
+            return "Game Complete"
+        } else {
+            return ""
+        }
     }
 
     return (
         <div className="App">
             <h1>Elden Ring Memory Game</h1>
-            <MemCards />
+            <h3>{gameStatus(completion)}</h3>
+            <MemCards
+                moveCounter={moveCounter} setMoveCounter={setMoveCounter}
+                completion={completion} setCompletion={setCompletion} />
+            <h2>Moves: {moveCounter}</h2>
             <button onClick={newGame}>New Game</button>
         </div>
     );
