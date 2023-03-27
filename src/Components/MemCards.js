a905f0586cfa3becb0874298984cabd60d7d1a72
ftrzcinka <80350380+ftrzcinka@users.noreply.github.com>
2023-03-27 12:43:39 -0400

    Update MemCards.js

--- src/Components/MemCards.js
+++ src/Components/MemCards.js
@@ -2,19 +2,22 @@ import { useState, useRef } from 'react'
 import MemCard from './MemCard'
 
 export default function MemCards({moveCounter, setMoveCounter, completion, setCompletion }) {
+
+    //Had to upload images to a server to be able to access them, because for some reason they were not showing up
+    //originally did img: '/ImageFolder/EldenRing_img1.jpg' which used to work locally, but not anymore
     const [cards, setCards] = useState([
-        { id: 0, num: 0, name: 'MountainTop', status: '', img: '/ImageFolder/EldenRing_img1.jpg' },
-        { id: 0, num: 1, name: 'MountainTop', status: '', img: '/ImageFolder/EldenRing_img1.jpg' },
-        { id: 1, num: 2, name: 'Capital', status: '', img: '/ImageFolder/EldenRing_img2.jpg' },
-        { id: 1, num: 3, name: 'Capital', status: '', img: '/ImageFolder/EldenRing_img2.jpg' },
-        { id: 2, num: 4, name: 'FarumAzula', status: '', img: '/ImageFolder/EldenRing_img3.jpg' },
-        { id: 2, num: 5, name: 'FaramaAzula', status: '', img: '/ImageFolder/EldenRing_img3.jpg' },
-        { id: 3, num: 6, name: 'Academy', status: '', img: '/ImageFolder/EldenRing_img4.jpg' },
-        { id: 3, num: 7, name: 'Academy', status: '', img: '/ImageFolder/EldenRing_img4.jpg' },
-        { id: 4, num: 8, name: 'AltusSteps', status: '', img: '/ImageFolder/EldenRing_img5.jpg' },
-        { id: 4, num: 9, name: 'AltusSteps', status: '', img: '/ImageFolder/EldenRing_img5.jpg' },
-        { id: 5, num: 10, name: 'Stormveil', status: '', img: '/ImageFolder/EldenRing_img6.jpg' },
-        { id: 5, num: 11, name: 'Stormveil', status: '', img: '/ImageFolder/EldenRing_img6.jpg' }
+        { id: 0, num: 0, name: 'MountainTop', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img1.jpg' },
+        { id: 0, num: 1, name: 'MountainTop', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img1.jpg' },
+        { id: 1, num: 2, name: 'Capital', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img2.jpg' },
+        { id: 1, num: 3, name: 'Capital', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img2.jpg' },
+        { id: 2, num: 4, name: 'FarumAzula', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img3.jpg' },
+        { id: 2, num: 5, name: 'FaramaAzula', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img3.jpg' },
+        { id: 3, num: 6, name: 'Academy', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img4.jpg' },
+        { id: 3, num: 7, name: 'Academy', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img4.jpg' },
+        { id: 4, num: 8, name: 'AltusSteps', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img5.jpg' },
+        { id: 4, num: 9, name: 'AltusSteps', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img5.jpg5' },
+        { id: 5, num: 10, name: 'Stormveil', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img6.jpg' },
+        { id: 5, num: 11, name: 'Stormveil', status: '', img: 'https://moodyrahman.com/files/filip/EldenRing_img6.jpg' }
     ].sort(() => Math.random() - 0.5))
 
     const [prev, setPrev] = useState(-1)
@@ -22,7 +25,7 @@ export default function MemCards({moveCounter, setMoveCounter, completion, setCo
     const [checking, setChecking] = useState(0)
 
     function check(current) {
-        if (cards[current].id == cards[prev].id) {
+        if (cards[current].id === cards[prev].id) {
             cards[current].stat = "correct"
             cards[prev].stat = "correct"
             setCompletion(completion+1)
@@ -44,7 +47,7 @@ export default function MemCards({moveCounter, setMoveCounter, completion, setCo
     }
 
     function handleClick(id) {
-        if (cards[id].stat != "correct") {
+        if (cards[id].stat !== "correct") {
             if (checking === 0) {
                 if (prev === -1) {
                     cards[id].stat = "active"
@@ -52,7 +55,7 @@ export default function MemCards({moveCounter, setMoveCounter, completion, setCo
                     setPrev(id)
                     setPrevNum(cards[id].num)
                 } else {
-                    if (prevNum != cards[id].num) {
+                    if (prevNum !== cards[id].num) {
                         setMoveCounter(moveCounter + 1)
                         setChecking(1)
                         check(id)
