7a28e58a5b31e81270e2d95fc38d7a865ff9968b
ftrzcinka <80350380+ftrzcinka@users.noreply.github.com>
2023-03-27 09:30:47 -0400

    Update MemCard.js

--- src/Components/MemCard.js
+++ src/Components/MemCard.js
@@ -1,1 +1,9 @@
+export default function MemCard({ card, id, handleClick }) {
+    const cardClass = card.stat ? " active " + card.stat : ""
 
+    return (
+        <div className={"card" + cardClass} onClick={() => handleClick(id) }>
+            <img src={card.img} alt={card.name} />
+        </div>
+    )
+}
